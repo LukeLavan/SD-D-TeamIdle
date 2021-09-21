7a3e4f90fa8e7d53cfece4d5e3538d51d1e6b844
Luke Lavan <luke.lavan01@gmail.com>
2021-09-21 17:07:21 -0400

    add basic (placeholder) idle mechanic
    just proof of concept, please replace everything here later

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,51 @@ import './App.css';
 
 function App(): JSX.Element {
   const [honey, setHoney] = useState(0);
+  const [bees, setBees] = useState(0);
+  const [costOfNextBee, setCostOfNextBee] = useState(1);
 
-  const increment = () => {
+  const incrementHoney = () => {
     setHoney(honey + 1);
   };
 
+  const incrementBees = () => {
+    if (honey >= costOfNextBee) {
+      setBees(bees + 1);
+      setHoney(honey - costOfNextBee);
+    }
+  };
+
+  // calculate new bee cost when bee count updates
   useEffect(() => {
-    console.log('hello world');
-  }, []);
+    calcCostOfNextBee();
+  }, [bees]);
+
+  const calcCostOfNextBee = () => {
+    console.log('current bee count: ' + bees);
+    setCostOfNextBee((bees + 1) ** 2);
+  };
+
+  const processTick = () => {
+    setHoney(honey + bees);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(processTick, 1000);
+    return () => clearInterval(timer);
+  });
 
   return (
     <div className="App">
       <h1>bee game</h1>
-      {honey} <br />
-      <button onClick={increment}>buzz buzz buzz</button>
+      <p>
+        Honey: {honey} <br />
+        <button onClick={incrementHoney}>buzz buzz buzz</button>
+      </p>
+      <p>
+        bees: {bees} <br />
+        <button onClick={incrementBees}>gain a bee!</button> <br />
+        cost of next bee: {costOfNextBee} <br />
+      </p>
     </div>
   );
 }
